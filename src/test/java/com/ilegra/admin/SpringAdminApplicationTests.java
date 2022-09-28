0303d0bf68b69c912b54dd545e1ed4a141a57bac
Marcelo Schambeck <marcelo.demoraesschambeck@thomsonreuters.com>
2022-09-28 19:46:31 -0300

    Heroku

--- src/test/java/com/ilegra/admin/SpringAdminApplicationTests.java
+++ src/test/java/com/ilegra/admin/SpringAdminApplicationTests.java
@@ -1,9 +1,9 @@
 package com.ilegra.admin;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringBootConfiguration;
 
-@SpringBootTest
+@SpringBootConfiguration
 class SpringAdminApplicationTests {
 
 	@Test
